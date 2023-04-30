0cbe5427955eca2dcdc34fbbbf3cf5d424c00870
Dmitriy Orlov <dimorl02@gmail.com>
2023-04-30 13:37:15 +0300

    f

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,37 @@
 let profile = document.querySelector('.profile');
 let popup = document.querySelector('.popup');
 let addpopup = document.querySelector('.add-popup');
-let elements = document.querySelectorAll('.elements__element');
 let info = profile.querySelector('.profile__info');
 let cardsList = document.querySelector('.cards');
-let card = document.querySelector('.card').content;
+let card = document.querySelector('.cards__card');
+let cardTemplate = document.querySelector('.cards__card-template').content;
 
 const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; 
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 
 
@@ -39,6 +39,8 @@ let form = popup.querySelector('.popup__form');
 let author = info.querySelector('.profile__author');
 let textname = author.querySelector('.profile__name');
 let textbrief = author.querySelector('.profile__brief');
+let cardname = addpopup.querySelector('.popup__name_type_name');
+let cardimage = addpopup.querySelector('.popup__name_type_image');
 
 let popupname = popup.querySelector('.popup__text_type_name');
 let popupbrief = popup.querySelector('.popup__text_type_brief');
@@ -48,62 +50,82 @@ let addButton = profile.querySelector('.profile__add-button');
 let editButton = profile.querySelector('.profile__edit-button');
 let closeButton = popup.querySelector('.popup__close-button');
 let secondcloseButton = addpopup.querySelector('.add-popup__close-button');
-
+let createButton = addpopup.querySelector('.add-popup__create-button');
 
 function openPopup() {
-    popupname.value = textname.textContent;
-    popupbrief.value = textbrief.textContent;
-    popup.classList.add('popup_opened'); 
+  popupname.value = textname.textContent;
+  popupbrief.value = textbrief.textContent;
+  popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-    popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 }
 
 function openAddPopup() {
-    addpopup.classList.add('add-popup_opened'); 
+  addpopup.classList.add('add-popup_opened');
 }
 
 function closeAddPopup() {
-    addpopup.classList.remove('add-popup_opened');
+  addpopup.classList.remove('add-popup_opened');
 }
-
 
 
 function editProfile(evt) {
-    evt.preventDefault();
-    textname.textContent = popupname.value;
-    textbrief.textContent = popupbrief.value;
-    closePopup();
+  evt.preventDefault();
+  textname.textContent = popupname.value;
+  textbrief.textContent = popupbrief.value;
+  closePopup();
 }
 
 
-elements.forEach(function (element) {
-    let likeButton = element.querySelector('.elements__like-button');
-    likeButton.addEventListener('click', function() {
-        likeButton.classList.toggle('elements__like-button_active');
-    });
-    
-});
+renderCards();
 
 
-initialCards.forEach(function (element) {
-    const cardElement = card.cloneNode(true);
+function addCard(evt) {
+  evt.preventDefault();
+  const name = cardname.value;
+  const link = cardimage.value;
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.cards__name').textContent = name;
+  cardElement.querySelector('.cards__image').src = link;
+  const newcard = { name, link };
+  cardsList.prepend(cardElement);
+  initialCards.unshift(newcard);
+
+  const LikeButton = card.querySelector('.cards__like-button');
+  LikeButton.addEventListener('click', () => {
+    LikeButton.classList.toggle('cards__like-button_active');
+  });
+
+  cardElement.querySelector('.cards__delete-button').addEventListener('click', () => {
+    initialCards.remove(newcard);
+  });
   
+
+  closeAddPopup();
+}
+
+function renderCards() {
+  initialCards.forEach(function (element) {
+    const cardElement = cardTemplate.cloneNode(true);
+
     cardElement.querySelector('.cards__name').textContent = element.name;
     cardElement.querySelector('.cards__image').src = element.link;
-  
+
     cardsList.append(cardElement);
-  })
+  });
+}
 
 
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 
-editButton.addEventListener('click', openPopup); 
-closeButton.addEventListener('click', closePopup); 
+addButton.addEventListener('click', openAddPopup);
+secondcloseButton.addEventListener('click', closeAddPopup);
+secondcloseButton.addEventListener('click', closeAddPopup);
 
-addButton.addEventListener('click', openAddPopup); 
-secondcloseButton.addEventListener('click', closeAddPopup); 
+createButton.addEventListener('click', addCard);
 
-form.addEventListener('submit', editProfile); 
- 
- 
+form.addEventListener('submit', editProfile);
+
